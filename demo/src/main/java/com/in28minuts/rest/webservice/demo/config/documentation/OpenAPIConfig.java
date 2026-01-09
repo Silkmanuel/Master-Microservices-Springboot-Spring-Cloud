393d4cd272da59cfad84450a9dfb63a767165b58
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-01-09 16:30:55 +0100

    Exploring Content Negotiation - Implementing Support for XML

---
+++ demo/src/main/java/com/in28minuts/rest/webservice/demo/config/documentation/OpenAPIConfig.java
@@ -0,0 +1,26 @@
+package com.in28minuts.rest.webservice.demo.config.documentation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI wendaOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info().title("Microservices with Spring Boot and Spring Cloud - Demo")
+                        .description("API documentation for - Demo Aplication")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                        .name("Suporte Microservices with Spring Boot and Spring Cloud - Demo")
+                        .url("https://silk.com/support")
+                        .email("silk552001@gmail.com"))
+                    );
+    }
+    
+}
